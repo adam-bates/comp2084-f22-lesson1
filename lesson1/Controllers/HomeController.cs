d61884623c4e824897571269d570d5d7ad8c3a79
adam-bates <adam@adambates.ca>
2022-09-09 22:19:54 -0400

    Upload lesson files.

---
+++ lesson1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using lesson1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lesson1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            // Can be returned as an IActionResult
+            // Error - 404, JSON, File, Redirect
+
+            return View();
+	    }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
